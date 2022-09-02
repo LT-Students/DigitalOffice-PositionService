31c2128ee92020740716c576b875407f83469c7f
Masha <mariyasin@list.ru>
2022-09-02 14:44:41 +0300

    fix

--- src/PositionService.Broker/Consumers/ActivateUserPositionConsumer.cs
+++ src/PositionService.Broker/Consumers/ActivateUserPositionConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using DigitalOffice.Models.Broker.Publishing;
+using LT.DigitalOffice.Kernel.RedisSupport.Helpers.Interfaces;
 using LT.DigitalOffice.PositionService.Data.Interfaces;
 using MassTransit;
 using Microsoft.Extensions.Logging;
@@ -10,13 +11,16 @@ namespace LT.DigitalOffice.PositionService.Broker.Consumers
   public class ActivateUserPositionConsumer : IConsumer<IActivateUserPublish>
   {
     private readonly IPositionUserRepository _repository;
+    private readonly IGlobalCacheRepository _globalCache;
     private readonly ILogger<ActivateUserPositionConsumer> _logger;
 
     public ActivateUserPositionConsumer(
       IPositionUserRepository repository,
+      IGlobalCacheRepository globalCache,
       ILogger<ActivateUserPositionConsumer> logger)
     {
       _repository = repository;
+      _globalCache = globalCache;
       _logger = logger;
     }
 
@@ -26,6 +30,8 @@ namespace LT.DigitalOffice.PositionService.Broker.Consumers
 
       if (positionId.HasValue)
       {
+        await _globalCache.RemoveAsync(positionId.Value);
+
         _logger.LogInformation("UserId '{UserId}' activated in positiontId '{PositionId}'", context.Message.UserId, positionId);
       }
     }
--- src/PositionService.Broker/Consumers/CreateUserPositionConsumer.cs
+++ src/PositionService.Broker/Consumers/CreateUserPositionConsumer.cs
@@ -1,8 +1,10 @@
 ﻿using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.RedisSupport.Helpers.Interfaces;
 using LT.DigitalOffice.Models.Broker.Publishing.Subscriber.Position;
 using LT.DigitalOffice.PositionService.Data.Interfaces;
 using LT.DigitalOffice.PositionService.Mappers.Db.Interfaces;
 using MassTransit;
+using Microsoft.Extensions.Logging;
 
 namespace LT.DigitalOffice.PositionService.Broker.Consumers
 {
@@ -11,15 +13,21 @@ namespace LT.DigitalOffice.PositionService.Broker.Consumers
     private readonly IPositionUserRepository _positionUserRepository;
     private readonly IPositionRepository _positionRepository;
     private readonly IDbPositionUserMapper _positionUserMapper;
+    private readonly IGlobalCacheRepository _globalCache;
+    private readonly ILogger<CreateUserPositionConsumer> _logger;
 
     public CreateUserPositionConsumer(
       IPositionUserRepository positionUserRepository,
       IPositionRepository positionRepository,
-      IDbPositionUserMapper positionUserMapper)
+      IDbPositionUserMapper positionUserMapper,
+      IGlobalCacheRepository globalCache,
+      ILogger<CreateUserPositionConsumer> logger)
     {
       _positionUserRepository = positionUserRepository;
       _positionRepository = positionRepository;
       _positionUserMapper = positionUserMapper;
+      _globalCache = globalCache;
+      _logger = logger;
     }
 
     public async Task Consume(ConsumeContext<ICreateUserPositionPublish> context)
@@ -27,6 +35,18 @@ namespace LT.DigitalOffice.PositionService.Broker.Consumers
       if (await _positionRepository.DoesExistAsync(context.Message.PositionId))
       {
         await _positionUserRepository.CreateAsync(_positionUserMapper.Map(context.Message));
+
+        if (context.Message.IsActive)
+        {
+          await _globalCache.RemoveAsync(context.Message.PositionId);
+        }
+      }
+      else
+      {
+        _logger.LogError(
+          "Cannot create userId '{UserId}' for departmentId '{DepartmentId}'",
+          context.Message.UserId,
+          context.Message.PositionId);
       }
     }
   }
